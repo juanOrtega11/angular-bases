59aa7f69874057a649197724b8c5d60c998cd552
Juan Ortega <juanddog99@gmail.com>
2023-12-24 17:52:10 -0600

    proyecto completo (error en eliminar item)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,20 @@ import {
 
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CounterModule, HeroesModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    CounterModule,
+    HeroesModule,
+    DbzModule,
+    AppRoutingModule,
+  ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
 })
---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
+
+@Injectable({ providedIn: 'root' })
+export class DbzService {
+  public characters: Character[] = [
+    {
+      id: uuid(),
+      name: 'Krillin',
+      power: 1000,
+    },
+    {
+      id: uuid(),
+      name: 'Goku',
+      power: 9500,
+    },
+  ];
+  onNewCharacter(characters: Character): void {
+    const newCharacter: Character = { id: uuid(), ...characters };
+    this.characters.push(characters);
+  }
+  onDeleteCharacter(id: string) {
+    this.characters = this.characters.filter(
+      (character) => character.id !== id
+    );
+  }
+}
